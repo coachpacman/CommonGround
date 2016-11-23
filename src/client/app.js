d27aa81173807b394c2067211a33fee54e352c80
Michael Sweeney <Michael@MacBook-Pro.local>
2016-11-23 13:51:10 -0800

    added exclamations

--- src/client/app.js
+++ src/client/app.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom'
 
 import 'normalize.scss/normalize.scss'
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('app'))
+ReactDOM.render(<h1>Hello World!!</h1>, document.getElementById('app'))
